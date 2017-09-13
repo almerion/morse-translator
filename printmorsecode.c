6930ebd933b2c7ee0e388412f660f3d0b2572b86
heari <h.emreari@protonmail.com>
2017-09-13 18:36:47 +0300

    Added comment to the source file.

--- printmorsecode.c
+++ printmorsecode.c
@@ -1,3 +1,13 @@
+/*------------------------------------------*
+ * Author        : heari                    *
+ * Created Date  : 9/9/17                   *
+ * Last Update   : 13/9/17                  *
+ * Description   : The function is using    *
+ *   for communicate with user. It prints   *
+ *   the morse codes to terminal.           *
+ *------------------------------------------*
+ */
+
 #include "morse.h"
 #include <stdio.h>
 
